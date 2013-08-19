15ac7a8b4e8f5f59ec1d02b65898818e6c60c8d9
Mikola Lysenko <mikolalysenko@gmail.com>
2013-08-19 18:29:26 -0500

    adding files

---
+++ README.md
@@ -0,0 +1,64 @@
+draw-billboard
+==============
+Draws a 2D billboarded sprite in WebGL.  Useful for debugging and small special effects.
+
+Example
+=======
+
+```javascript
+var shell = require("gl-now")()
+var camera = require("game-shell-orbit-camera")(shell)
+var renderText = require("gl-render-text")
+var drawBillboard = require("draw-billboard")
+var mat4 = require("gl-matrix").mat4
+
+var texture
+var positions = new Array(100)
+
+shell.on("gl-init", function() {
+  var gl = shell.gl
+  texture = renderText(gl, "Billboard")
+  
+  for(var i=0; i<100; ++i) {
+    positions[i] = [ 100 * (0.5 - Math.random()),
+                     100 * (0.5 - Math.random()),
+                     100 * (0.5 - Math.random()) ]
+  }
+})
+
+shell.on("gl-render", function() {
+  var proj = mat4.perspective(mat4.create(), Math.PI/4.0, shell.width/shell.height, 0.1, 1000.0)
+  var view = camera.view()
+  for(var i=0; i<100; ++i) {
+    drawBillboard(shell.gl, positions[i], { texture: texture, projection: proj, view: view })
+  }
+})
+```
+
+Try it in your browser.
+
+Install
+=======
+
+    npm install draw-billboard
+    
+API
+===
+
+### `require("draw-billboard")(gl, position[, options])`
+Draws a billboard at the given position.
+
+* `gl` is a WebGL context
+* `positions` is the position of the billboarded sprite
+* `options` is an object containing the following properties:
+    + `texture` A WebGL texture object
+    + `lo` Lower texture coordinate
+    + `hi` Upper texture coordiante
+    + `width` Width of billboard to draw
+    + `height` Height of billboard to draw
+    + `model` Model matrix for billboard
+    + `view` View matrix for billboard
+    + `projection` Projection matrix for billboard
+    
+# Credits
+(c) 2013 Mikola Lysenko. MIT License
---
+++ billboard.js
@@ -0,0 +1,75 @@
+"use strict"
+
+var createShader = require("gl-shader")
+var createMesh = require("gl-mesh")
+var glm = require("gl-matrix")
+var mat4 = glm.mat4
+
+function createBillboardMesh(gl) {
+  var mesh = createMesh(gl, [[0,1,2], [1,2,3]], {
+    "uv": [[-1,-1], [-1,1], [1,-1], [1,1]]
+  })
+  gl.__BILLBOARD_MESH = mesh
+  return mesh
+}
+
+function createBillboardShader(gl) {
+  var shader = createShader(gl,
+  "attribute vec2 uv;\
+  uniform vec3 position;\
+  uniform mat4 model;\
+  uniform mat4 view;\
+  uniform mat4 projection;\
+  uniform vec2 lo;\
+  uniform vec2 hi;\
+  uniform float width;\
+  uniform float height;\
+  varying vec2 tc;\
+  void main() {\
+    vec4 mposition = vec4(uv, 0.0, 0.0) + view * model * vec4(position, 1.0);\
+    gl_Position = projection * mposition;\
+    tc = (0.5 * vec2(1.0+uv.x,1.0-uv.y) - lo) * (hi - lo);\
+  }",
+  "precision highp float;\
+  uniform sampler2D texture;\
+  varying vec2 tc;\
+  void main() {\
+    gl_FragColor = texture2D(texture, tc);\
+  }")
+  gl.__BILLBOARD_SHADER = shader
+  return shader
+}
+
+function drawBillboard(gl, position, options) {
+  position = position || [0,0,0]
+  options = options || {}
+  
+  var texture = options.texture || 0
+  var lo = options.lo || [0,0]
+  var hi = options.hi || [1,1]
+  var width = options.width || 1.0
+  var height = options.height || 1.0
+  var model = options.model || mat4.identity(mat4.create())
+  var view = options.view || mat4.identity(mat4.create())
+  var projection = options.projection || mat4.identity(mat4.create())
+  
+  var shader = gl.__BILLBOARD_SHADER || createBillboardShader(gl)
+  var mesh = gl.__BILLBOARD_MESH || createBillboardMesh(gl)
+  
+  shader.bind()
+  shader.uniforms.position = position
+  shader.uniforms.lo = lo
+  shader.uniforms.hi = hi
+  shader.uniforms.width = width
+  shader.uniforms.height = height
+  shader.uniforms.texture = texture
+  shader.uniforms.model = model
+  shader.uniforms.view = view
+  shader.uniforms.projection = projection
+  
+  mesh.bind(shader)
+  mesh.draw()
+  mesh.unbind()
+}
+
+module.exports = drawBillboard
